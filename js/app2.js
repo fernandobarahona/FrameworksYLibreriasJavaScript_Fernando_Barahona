61fe55b5a84278f4f95cc72ad7740cd9c5dcc462
Fernando Barahona <fernandobarahonad@gmail.com>
2019-03-15 15:17:36 -0500

    Avance y cambio de app.js
    Avance full y ya esta casi listo pero hay varios errores, como el cambio de clase al hacer el cambio de caramelo(lo que provoca que no se eliminen los nuevos repetidos).
    
    Por esto cree una nueva app.js y aca voy a repetir de cero todo para estar mas ordenado

--- js/app2.js
+++ js/app2.js
@@ -1,5 +1,7 @@
 $( document ).ready(function() {
     'use strict'
+    var contadorFilas = 0;
+    var cantidadFaltanteCaramelosFila = [];
     //funcion para animar MATCH
     var animacionMatch = function(){
         $('.main-titulo').animate({color: '#DCFF0E'}, 300, function () {
@@ -9,41 +11,27 @@ $( document ).ready(function() {
     };
     //funcion para llenar de caramelos el tablero
     var inicioDeTablero = function(){
-        for(var jj = 1; jj <= 7 ; jj++){
-            for(var ii = 0; ii < 7 ; ii++){
-                var identificadorImagen = Math.floor((Math.random() * 4) + 1);
-                $('.col-'+jj).append('<img class="imagen'+identificadorImagen+'" style="height: calc(680px/7);" src="/image/'+identificadorImagen+'.png">');
-            }
-        }
+        contadorFilas = 0;
+        setInterval(function(){inicioDeTableroAux();}, 200);
     };
+    var inicioDeTableroAux = function () {
+        if(contadorFilas < 7){
+            for(var jj = 1; jj <= 7 ; jj++){
+                var identificadorImagen = Math.floor((Math.random() * 4) + 1);
+                $('.col-'+jj).append('<img class="imagen'+identificadorImagen+'" data-validar="noRepetido" style="height: calc(680px/7);" src="/image/'+identificadorImagen+'.png">');
+            }
+            contadorFilas++;
+        }
+    }
     //funcion para buscar y eliminar si hay 3 o mas seguidos de un mismo
     var buscarCoincidencias = function(){
         detectarCadenaVertical();
         detectarCadenaHorizontal();
         detectarElesVertical();
         detectarElesHorizontal();
+        //imprimir(1);
         eliminarCadenas();
-        rellenarVacios();  
-    };
-    var rellenarVacios = function(){
-        $('html').delay(1200).queue(function () {
-            for(var jj = 1; jj <= 7 ; jj++){
-                if($('.col-'+jj).children().length != 7){
-                    var cantidadFaltante = 7-$('.col-'+jj).children().length;
-                    console.log(cantidadFaltante);
-                    for(var ii = 0; ii < cantidadFaltante; ii++){
-                        var identificadorImagen = Math.floor((Math.random() * 4) + 1);
-                        $('.col-'+jj).append('<img class="imagen'+identificadorImagen+'" style="height: calc(680px/7);" src="/image/'+identificadorImagen+'.png">');
-                    }
-                }
-            };
-            for(var jj = 1; jj <= 7 ; jj++){
-                for(var ii = 0; ii < 7 ; ii++){
-                    $('.col-'+jj+' img:eq('+ii+')'),$('.col-'+jj+' img:eq('+ii+')').attr('data-validar','No-repet');
-                };
-            };
-        });
-
+        setTimeout(function(){rellenarVacios();} ,700);
     };
     var detectarCadenaVertical = function(){
         for(var jj = 1; jj <= 7 ; jj++){
@@ -91,13 +79,47 @@ $( document ).ready(function() {
             };
         };
     };
+    var imprimir = function (x) {
+        console.log(x+'--------------------------------------------------------------------------------------------');        
+        for(var jj = 1; jj <= 7 ; jj++){
+            console.log('-----------------COL'+jj+'----------------------');
+            for(var ii = 0; ii < 7 ; ii++){
+                console.log($('.col-'+jj+' img:eq('+ii+')'),$('.col-'+jj+' img:eq('+ii+')').attr('data-validar'));
+            };
+        };
+    };
     var eliminarCadenas = function(){
         for(var jj = 1; jj <= 7 ; jj++){
             for(var ii = 0; ii < 7 ; ii++){
-                console.log($('.col-'+jj+' img:eq('+ii+')'),$('.col-'+jj+' img:eq('+ii+')').attr('data-validar'));
                 if($('.col-'+jj+' img:eq('+ii+')').attr('data-validar') == 'repetido'){
-                    $('.col-'+jj+' img:eq('+ii+')').animate({backgroundColor: 'yellow'}).delay(650).queue(function() {$(this).remove();} );
-                }
+                    $('.col-'+jj+' img:eq('+ii+')').hide("pulsate",600,function(){$(this).remove();});
+                };
+            };
+        };
+    };
+    var rellenarVacios = function(){
+        for(var jj = 0; jj < 7 ; jj++){
+                cantidadFaltanteCaramelosFila[jj] = 7-$('.col-'+(jj+1)).children().length;
+                //console.log(cantidadFaltanteCaramelosFila);
+        };
+        setInterval(function(){rellenarVaciosAux();},200);
+    };
+    var rellenarVaciosAux = function(){
+      
+        for (var jj = 0; jj < 7; jj++){
+            if (cantidadFaltanteCaramelosFila[jj] >0){
+            //    console.log(cantidadFaltanteCaramelosFila[jj],'anadir');
+                var identificadorImagen = Math.floor((Math.random() * 4) + 1);
+                $('.col-'+(jj+1)).prepend('<img class="imagen'+identificadorImagen+'" style="height: calc(680px/7);" src="/image/'+identificadorImagen+'.png">');
+                cantidadFaltanteCaramelosFila[jj]--;
+            };
+        };
+        resetearRepetidos();
+    };
+    var resetearRepetidos = function(){
+        for(var jj = 1; jj <= 7 ; jj++){
+            for(var ii = 0; ii < 7 ; ii++){
+                $('.col-'+jj+' img:eq('+ii+')'),$('.col-'+jj+' img:eq('+ii+')').attr('data-validar','noRepetido');
             };
         };
     };
@@ -111,12 +133,54 @@ $( document ).ready(function() {
                 duration: '2m'	
             });
             buscarCoincidencias();
+            setTimeout(sensarMovimientos,1500)
         });
     };
+    var sensarMovimientos = function(){
+        $('img').draggable({
+            containment: '.panel-tablero',
+            droppable: 'img',
+            revert: true,
+            revertDuration: 500,
+            grid: [100, 100],
+            zIndex: 10,
+            drag: restingirMovimientos
+        });
+        $('img').droppable({
+            drop: swapCandy
+        });    
+    }
+    var restingirMovimientos = function(event, candyDrag) {
+        candyDrag.position.top = Math.min(100, candyDrag.position.top);
+        candyDrag.position.bottom = Math.min(100, candyDrag.position.bottom);
+        candyDrag.position.left = Math.min(100, candyDrag.position.left);
+        candyDrag.position.right = Math.min(100, candyDrag.position.right);
+    }
+    function swapCandy(event, candyDrag) {
+        console.log(candyDrag, $(candyDrag.draggable).attr('src'), $(candyDrag.draggable).parent());
+        
+        var candyDrag = $(candyDrag.draggable);
+        var dragSrc = candyDrag.attr('src');
+        var candyDrop = $(this);
+        var dropSrc = candyDrop.attr('src');
+        var dragClass = candyDrag.attr('class');
+        var dropClass = candyDrop.attr('class');
+        candyDrag.attr('class', dropClass);
+        candyDrop.attr('class', dragClass);
+        candyDrag.attr('src', dropSrc);
+        candyDrop.attr('src', dragSrc);
+        setTimeout(function () {
+            buscarCoincidencias();
+        }, 800);
+    
+    }
+
     //inicializar el programa
     animacionMatch();
     inicioDeTablero();
     sensarBotonStart();
-
-
+    //  sensarMovimientos();
+    window.imprimir = function(){ imprimir()};
+    window.rellenarVacios = function(){rellenarVacios()};
+    window.buscarCoincidencias = function(){buscarCoincidencias()};
 });
